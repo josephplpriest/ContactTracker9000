b40531433c5b505e46cac3eb087164c737587ebe
Paul Priest <josephplpriest@gmail.com>
2024-05-29 07:32:00 -0400

    add dev http logging

--- ContactTracker9000/Program.cs
+++ ContactTracker9000/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.HttpLogging;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts => 
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
+app.MapGet("/", () => "Hi Paul");
+
+app.MapGet("/contact", () => new Person("David", "Priest"));
 
 app.Run();
+
+public record Person(string FirstName, string LastName);

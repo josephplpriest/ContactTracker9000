29941ddf69f0ed13e05e2220e9ef84964afb23ee
Paul Priest <josephplpriest@gmail.com>
2024-06-17 09:09:31 -0400

    data object testing

---
+++ app/ContactTracker.Data/Model.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactTracker.Domain.Contacts;
+using ContactTracker.Domain.Events;
+public class ContactTrackerContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Event> Events { get; set; }
+
+    public string DbPath { get; }
+
+    public ContactTrackerContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "ContactTracker.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
---
+++ app/ContactTracker.Data/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using ContactTracker.Domain.Contacts;
+using ContactTracker.Domain.Events;
+
+var contactJson = File.ReadAllText("TestContact.json");
+
+var c = JsonSerializer.Deserialize<Contact>(contactJson);
+
+var eventJson = File.ReadAllText("TestEvent.json");
+
+var e = JsonSerializer.Deserialize<Event>(eventJson);
+
+using var db = new ContactTrackerContext();
+
+Console.WriteLine("Inserting a new event");
+db.Add(e);
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for an event");
+var ev = db.Events
+    .OrderBy(e => e.Type)
+    .First();
+
+Console.WriteLine($"{JsonSerializer.Serialize(ev)}");

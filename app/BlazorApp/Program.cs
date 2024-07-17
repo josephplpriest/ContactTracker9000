f938b22f36932b0030db028d85d4567970f6df32
Paul Priest <Josephplpriest@gmail.com>
2024-07-17 18:41:57 -0400

    add blazor app

---
+++ app/BlazorApp/Program.cs
@@ -0,0 +1,31 @@
+using BlazorApp.Components;
+using ContactTracker.Domain;
+using ContactTracker.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<ContactContext>(opt => opt.UseSqlite($"Data Source ={nameof(ContactContext.ContactsDb)}.db"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
